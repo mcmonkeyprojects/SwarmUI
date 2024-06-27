57639902e97bdc894784abda3905577c16b2d304
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-06-27 09:53:17 -0700

    revert last
    actually this already happens (via ModelPathsChanged event) and i'm extremely confused how users are getting bit here??

--- src/WebAPI/AdminAPI.cs
+++ src/WebAPI/AdminAPI.cs
@@ -127,7 +127,6 @@ public static class AdminAPI
             Program.BuildModelLists();
             Program.RefreshAllModelSets();
             Program.ModelPathsChangedEvent?.Invoke();
-            await Program.Backends.ReloadAllBackends();
         }
         Program.ReapplySettings();
         return new JObject() { ["success"] = true };
