4f07f74e7dcbaa96c95d83e10817478e531e6272
maedtb <maedtb@gmail.com>
2024-07-01 16:16:12 -0400

    Update install-windows.bat to not exit command prompt on error (#37)
    The '/b' switch will exit the current script, but not close the command prompt.

--- install-windows.bat
+++ install-windows.bat
@@ -5,13 +5,13 @@ cd /d "%~dp0"
 if exist SwarmUI (
     echo SwarmUI is already installed in this folder. If this is incorrect, delete the 'SwarmUI' folder and try again.
     pause
-    exit
+    exit /b
 )
 
 if exist SwarmUI.sln (
     echo SwarmUI is already installed in this folder. If this is incorrect, delete 'SwarmUI.sln' and try again.
     pause
-    exit
+    exit /b
 )
 
 winget install Microsoft.DotNet.SDK.8 --accept-source-agreements --accept-package-agreements
