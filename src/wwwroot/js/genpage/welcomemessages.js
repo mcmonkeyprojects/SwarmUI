e6645d74babf5b18dcd5f14392a504e8513d5b45
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-07-08 06:53:35 -0700

    Swarm 0.9.1

--- src/wwwroot/js/genpage/welcomemessages.js
+++ src/wwwroot/js/genpage/welcomemessages.js
@@ -55,7 +55,8 @@ function automaticWelcomeMessage(override = null) {
         `New feature (2024-06-02): TensorRT Support!\nFor making things go fasterer! <a href="https://github.com/Stability-AI/StableSwarmUI/discussions/11#discussioncomment-9641683">(Feature Announcement Link)</a>`,
         `New feature (2024-06-12): SD3-Medium!\nCool new model architecture! <a href="https://new.reddit.com/r/StableDiffusion/comments/1de65iz">(Reddit Post Link)</a>`,
         /* Version release notes */
-        `Release notes (2024-06-21): Check out the <a href="https://github.com/mcmonkeyprojects/SwarmUI/releases/tag/0.9.0-Beta">Release Notes for version 0.9.0 (Beta)</a>`
+        `Release notes (2024-06-21): Check out the <a href="https://github.com/mcmonkeyprojects/SwarmUI/releases/tag/0.9.0-Beta">Release Notes for version 0.9.0 (Beta)</a>`,
+        `Release notes (2024-08-08): Check out the <a href="https://github.com/mcmonkeyprojects/SwarmUI/releases/tag/0.9.1-Beta">Release Notes for version 0.9.1 (Beta)</a>`
     ];
     let dotnetNotice = document.getElementById('dotnet_missing_message');
     if (dotnetNotice) {
