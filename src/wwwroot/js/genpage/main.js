17fa7b8894571b8fcc860126f727232a8ec59e94
maedtb <maedtb@gmail.com>
2024-07-10 08:10:18 -0400

    Add Remove Image buttons to ReVision/IPAdapter prompt images Users may now remove specific uploaded prompt images by clicking a 'X' button in the top right of images. Previously it was only possible to remove all uploaded images.
    See #61 for more details.

--- src/wwwroot/js/genpage/main.js
+++ src/wwwroot/js/genpage/main.js
@@ -1791,14 +1791,26 @@ function revisionAddImage(file) {
     let reader = new FileReader();
     reader.onload = (e) => {
         let data = e.target.result;
+        let imageContainer = createDiv(null, 'alt-prompt-image-container');
+        let imageRemoveButton = createSpan(null, 'alt-prompt-image-container-remove-button', '&times;');
+        imageRemoveButton.addEventListener('click', (e) => {
+            e.currentTarget.parentElement.remove();
+            autoRevealRevision();
+            altPromptSizeHandleFunc();
+        }, {
+            once: true
+        });
+        imageRemoveButton.title = 'Remove this image';
+        imageContainer.appendChild(imageRemoveButton);
         let imageObject = new Image();
         imageObject.src = data;
         imageObject.height = 128;
-        imageObject.className = 'alt-prompt-image';
+        imageObject.className = 'alt-prompt-image-container-image';
         imageObject.dataset.filedata = data;
+        imageContainer.appendChild(imageObject);
         clearButton.style.display = '';
         showRevisionInputs(true);
-        promptImageArea.appendChild(imageObject);
+        promptImageArea.appendChild(imageContainer);
         altPromptSizeHandleFunc();
     };
     reader.readAsDataURL(file);
