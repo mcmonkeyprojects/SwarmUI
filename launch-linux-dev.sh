de2bf4638d5d74e190acf2322baf6436ca7cfd82
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-07-12 17:50:01 -0700

    add launch-linux-dev

---
+++ launch-linux-dev.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Ensure correct local path.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
+
+# Add dotnet non-admin-install to path
+export PATH="$SCRIPT_DIR/.dotnet:~/.dotnet:$PATH"
+
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+
+# Cycle build folder forward
+rm -rf ./src/bin/live_release_backup
+mv ./src/bin/live_release ./src/bin/live_release_backup
+rm ./src/bin/must_rebuild
+rm src/bin/last_build
+
+# Build the program
+dotnet build src/SwarmUI.csproj --configuration Release -o ./src/bin/live_release
+
+# Default env configuration, gets overwritten by the C# code's settings handler
+export ASPNETCORE_ENVIRONMENT="Production"
+export ASPNETCORE_URLS="http://*:7801"
+# Actual runner.
+dotnet src/bin/live_release/SwarmUI.dll $@
