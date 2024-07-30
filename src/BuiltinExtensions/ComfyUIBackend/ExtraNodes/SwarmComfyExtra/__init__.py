de982108f5245ffc439bad6c29f7bc78ef677218
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-07-29 19:35:21 -0700

    SwarmComfyExtra: try/except

--- src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyExtra/__init__.py
+++ src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyExtra/__init__.py
@@ -1,7 +1,20 @@
-from . import SwarmRemBg, SwarmSaveAnimationWS, SwarmYolo
+NODE_CLASS_MAPPINGS = {}
 
-NODE_CLASS_MAPPINGS = (
-    SwarmRemBg.NODE_CLASS_MAPPINGS
-    | SwarmSaveAnimationWS.NODE_CLASS_MAPPINGS
-    | SwarmYolo.NODE_CLASS_MAPPINGS
-)
+# RemBg doesn't work on all python versions and OS's
+try:
+    from . import SwarmRemBg
+    NODE_CLASS_MAPPINGS.update(SwarmRemBg.NODE_CLASS_MAPPINGS)
+except ImportError:
+    pass
+# This uses FFMPEG which doesn't install itself properly on Macs I guess?
+try:
+    from . import SwarmSaveAnimationWS
+    NODE_CLASS_MAPPINGS.update(SwarmSaveAnimationWS.NODE_CLASS_MAPPINGS)
+except ImportError:
+    pass
+# Yolo uses Ultralytics, which is cursed
+try:
+    from . import SwarmYolo
+    NODE_CLASS_MAPPINGS.update(SwarmYolo.NODE_CLASS_MAPPINGS)
+except ImportError:
+    pass
