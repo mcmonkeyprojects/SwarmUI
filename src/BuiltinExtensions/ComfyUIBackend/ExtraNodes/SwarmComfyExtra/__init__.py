67a7b6358ce2d2eefe3b435c5c04854cdbb546d0
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-07-29 19:36:39 -0700

    add error messages too

--- src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyExtra/__init__.py
+++ src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyExtra/__init__.py
@@ -5,16 +5,19 @@ try:
     from . import SwarmRemBg
     NODE_CLASS_MAPPINGS.update(SwarmRemBg.NODE_CLASS_MAPPINGS)
 except ImportError:
+    print("Error: [Swarm] RemBg not available")
     pass
 # This uses FFMPEG which doesn't install itself properly on Macs I guess?
 try:
     from . import SwarmSaveAnimationWS
     NODE_CLASS_MAPPINGS.update(SwarmSaveAnimationWS.NODE_CLASS_MAPPINGS)
 except ImportError:
+    print("Error: [Swarm] SaveAnimationWS not available")
     pass
 # Yolo uses Ultralytics, which is cursed
 try:
     from . import SwarmYolo
     NODE_CLASS_MAPPINGS.update(SwarmYolo.NODE_CLASS_MAPPINGS)
 except ImportError:
+    print("Error: [Swarm] Yolo not available")
     pass
