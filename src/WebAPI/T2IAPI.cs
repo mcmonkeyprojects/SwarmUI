c045c4608e6aed172d8d300b551d42871477b33a
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-08-03 17:38:43 -0700

    Image MiniGrid: slightly better manual shapes for low number counts

--- src/WebAPI/T2IAPI.cs
+++ src/WebAPI/T2IAPI.cs
@@ -308,6 +308,10 @@ public static class T2IAPI
             {
                 rows--;
             }
+            if (imgs.Length <= GridShapeTable.Length)
+            {
+                (rows, columns) = GridShapeTable[imgs.Length - 1];
+            }
             int widthPerImage = imgs.Max(i => i.Width);
             int heightPerImage = imgs.Max(i => i.Height);
             ISImageRGBA grid = new(widthPerImage * columns, heightPerImage * rows);
@@ -329,6 +333,22 @@ public static class T2IAPI
         T2IEngine.PostBatchEvent?.Invoke(new(user_input, [.. griddables]));
         output(new JObject() { ["discard_indices"] = JToken.FromObject(discard) });
     }
+
+    public static (int, int)[] GridShapeTable =
+        [
+            (1, 1), // 1
+            (2, 1), // 2
+            (3, 1), // 3
+            (2, 2), // 4
+            (3, 2), // 5
+            (3, 2), // 6
+            (4, 2), // 7
+            (4, 2), // 8
+            (3, 3), // 9
+            (5, 2), // 10
+            (4, 3), // 11
+            (4, 3), // 12
+        ];
 
     [API.APIDescription("Takes an image and stores it directly in the user's history.\nBehaves identical to GenerateText2Image but never queues a generation.",
         """
