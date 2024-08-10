9875437c066a614db4b530d5d33a0117821655dd
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-08-10 13:20:28 -0700

    hide swarm version cornerpiece on comfy tab

--- src/wwwroot/js/genpage/main.js
+++ src/wwwroot/js/genpage/main.js
@@ -2194,6 +2194,16 @@ function storeImageToHistoryWithCurrentParams(img) {
     });
 }
 
+$('#toptablist').on('shown.bs.tab', function (e) {
+    let versionDisp = getRequiredElementById('version_display');
+    if (e.target.id == 'maintab_comfyworkflow') {
+        versionDisp.style.display = 'none';
+    }
+    else {
+        versionDisp.style.display = '';
+    }
+});
+
 function genpageLoad() {
     console.log('Load page...');
     window.imageEditor = new ImageEditor(getRequiredElementById('image_editor_input'), true, true, () => setPageBarsFunc(), () => needsNewPreview());
