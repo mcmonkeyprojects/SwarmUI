7ae034fd51651c83ea915e1210d0dfc89817760c
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-10-14 17:39:21 -0700

    experimental new bash script to file dotnet_root properly
    why the hell this is not automated by dotnet itself is beyond me

--- launchtools/linux-path-fix.sh
+++ launchtools/linux-path-fix.sh
@@ -1,16 +1,38 @@
 #!/usr/bin/env bash
 
-# Add dotnet non-admin-install to path
-export PATH="$SCRIPT_DIR/.dotnet:$HOME/.dotnet:$PATH"
-
-# Set the expected runtime root
-if [ -d "$SCRIPT_DIR/.dotnet" ]; then
-    export DOTNET_ROOT="$SCRIPT_DIR/.dotnet"
-    export DOTNET_ROOT_X64="$SCRIPT_DIR/.dotnet"
-elif [ -d "$HOME/.dotnet" ]; then
-    export DOTNET_ROOT="$HOME/.dotnet"
-    export DOTNET_ROOT_X64="$HOME/.dotnet"
-fi
-
+# No telemetry, no localization
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+
+# Add dotnet non-admin-install to path
+export PATH="$SCRIPT_DIR/.dotnet:$HOME/.dotnet:/usr/lib/dotnet:/usr/share/dotnet:$PATH"
+
+# Try to set the expected runtime root from expected locations
+expected_location=(
+    "$SCRIPT_DIR/.dotnet"
+    "$HOME/.dotnet"
+    "/usr/lib/dotnet"
+    "/usr/share/dotnet"
+)
+
+for location in "${expected_location[@]}"; do
+    if [ -d "$location" ]; then
+        export DOTNET_ROOT="$location"
+        export DOTNET_ROOT_X64="$location"
+        break
+    fi
+done
+
+# Fallback to trying to parse dotnet cli outputs
+if [ -z "$DOTNET_ROOT" ]; then
+    runtime_path=$(dotnet --list-runtimes | head -n 1 | awk -F'[\\[\\]]' '{print $2}')
+    if [ -d "$runtime_path" ]; then
+        actual_path="$(dirname "$(dirname "$runtime_path")")"
+        export DOTNET_ROOT="$actual_path"
+        export DOTNET_ROOT_X64="$actual_path"
+    fi
+fi
+
+if [ -z "$DOTNET_ROOT" ]; then
+    echo "Could not find dotnet runtime path, please report on Discord @ https://discord.gg/q2y38cqjNw with info about your dotnet installation"
+fi
