fabeab6bb8e6b5c963386280706b0913d5a8d582
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-11-15 09:54:10 -0800

    'popover notice' js util

--- src/wwwroot/js/genpage/ui_improvements.js
+++ src/wwwroot/js/genpage/ui_improvements.js
@@ -496,3 +496,22 @@ function doPopover(id, e) {
         e.stopImmediatePropagation();
     }
 }
+
+/** Shows a notice popover with the given text and color. */
+function doNoticePopover(text, className, targetX = mouseX, targetY = mouseY) {
+    let pop = createDiv(null, `sui-popover sui_popover_model ${className} sui-popover-notice`);
+    pop.style.width = '200px';
+    let x = Math.min(targetX, window.innerWidth - pop.offsetWidth - 10);
+    let y = Math.min(targetY, window.innerHeight - pop.offsetHeight);
+    pop.style.left = `${x}px`;
+    pop.style.top = `${y}px`;
+    pop.style.width = '';
+    pop.innerText = translate(text);
+    document.body.appendChild(pop);
+    setTimeout(() => {
+        pop.classList.add('sui-popover-notice-fade-1s');
+        setTimeout(() => {
+            pop.remove();
+        }, 1500);
+    }, 1000);
+}
