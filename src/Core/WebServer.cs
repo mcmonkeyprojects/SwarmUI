abbf36795715c51f99e623c99f8a4728efbf8995
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-11-18 14:13:40 -0800

    fix names

--- src/Core/WebServer.cs
+++ src/Core/WebServer.cs
@@ -97,8 +97,8 @@ public class WebServer
         RegisterTheme(new("cyber_swarm", "Cyber Swarm", ["/css/themes/cyber_swarm.css"], true));
         RegisterTheme(new("punked", "Punked", ["/css/themes/punked.css"], true));
         RegisterTheme(new("eyesear_white", "Eyesear White", ["/css/themes/eyesear_white.css"], false));
-        RegisterTheme(new("minimal_swarmpunk", "Minimal Swarm Punk", ["/css/themes/modern.css", "/css/themes/Minimal_swarmpunk.css"], true));
-        RegisterTheme(new("minimal_beweish", "Minimal Beweish", ["/css/themes/modern.css", "/css/themes/minimal_beweish.css"], true));
+        RegisterTheme(new("swarmpunk", "Swarm Punk", ["/css/themes/modern.css", "/css/themes/swarmpunk.css"], true));
+        RegisterTheme(new("beweish", "Beweish", ["/css/themes/modern.css", "/css/themes/beweish.css"], true));
     }
 
     /// <summary>Main prep, called by <see cref="Program"/>, generally should not be touched externally.</summary>
