c67a44dcce27c1d18ccc3b7cc0ffeffc857b88e9
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-12-10 20:08:58 -0800

    windows shortcut: also add to onedrive desktop if present

--- make-shortcut.bat
+++ make-shortcut.bat
@@ -1,7 +1,20 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
 set SHORTCUTPATH="%userprofile%\Desktop\SwarmUI.url"
-echo [InternetShortcut] >> "%SHORTCUTPATH%"
-echo URL="%CD%\launch-windows.bat" >> "%SHORTCUTPATH%"
-echo IconFile="%CD%\src\wwwroot\favicon.ico" >> "%SHORTCUTPATH%"
-echo IconIndex=0 >> "%SHORTCUTPATH%"
+if exist "%userprofile%\Desktop" (
+    del "%SHORTCUTPATH%"
+    echo [InternetShortcut] >> "%SHORTCUTPATH%"
+    echo URL="%CD%\launch-windows.bat" >> "%SHORTCUTPATH%"
+    echo IconFile="%CD%\src\wwwroot\favicon.ico" >> "%SHORTCUTPATH%"
+    echo IconIndex=0 >> "%SHORTCUTPATH%"
+)
+
+set SHORTCUTPATH="%userprofile%\Onedrive\Desktop\SwarmUI.url"
+if exist "%userprofile%\Onedrive\Desktop" (
+    del "%SHORTCUTPATH%"
+    echo [InternetShortcut] >> "%SHORTCUTPATH%"
+    echo URL="%CD%\launch-windows.bat" >> "%SHORTCUTPATH%"
+    echo IconFile="%CD%\src\wwwroot\favicon.ico" >> "%SHORTCUTPATH%"
+    echo IconIndex=0 >> "%SHORTCUTPATH%"
+)
