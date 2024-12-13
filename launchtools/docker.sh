2b0168a9ce2c630452f386df6a016ef78af738e3
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-12-13 01:42:50 -0800

    never use git cli
    (I used git cli so I could update-index +x on the scripts, naturally i miss the removal from the commit)

--- launchtools/docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Launch as normal, just ensure launch mode is off and host is global (to expose it out of the container)
-bash ./launch-linux.sh --launch_mode none --host 0.0.0.0
