a8ac2e4157a6ae0ecb8632512d86764d1ed51860
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-12-13 02:47:57 -0800

    docker: chmod g+s

--- launchtools/docker-open-inner.sh
+++ launchtools/docker-open-inner.sh
@@ -4,5 +4,9 @@
 
 cd /SwarmUI
 
+# Docker instance runs as root, so tell it to copy the group owner of folders to hopefully make it more accessible.
+# Unfortunately, "u+s" does not do the same thing for copying the user for some reason, so we're stuck with root owning files (maybe we could jankily direct port the host's uid? but, ew.)
+chmod g+s /SwarmUI /SwarmUI/**/
+
 # Launch as normal, just ensure launch mode is off and host is global (to expose it out of the container)
 bash /SwarmUI/launch-linux.sh --launch_mode none --host 0.0.0.0
