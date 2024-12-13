59c0318cdb7fd6d11749006a528889a3efcb5caa
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-12-13 01:25:11 -0800

    Move launch-docker to subdir

--- launch-docker.sh
+++ launch-docker.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-# Note: This is an example file, do not edit `launch-docker.sh`. Instead, duplicate the file and edit your duplicate.
-# `custom-launch-docker.sh` is reserved in gitignore for if you want to use that.
+echo WARNING: Deprecated legacy script. Will be removed soon. Use "./launchtools/launch-docker.sh" instead.
 
-docker build -f launchtools/StandardDockerfile.docker -t swarmui .
+sleep 15
 
-# add "--network=host" if you want to access other services on the host network (eg a separated comfy instance)
-docker run -it \
-    --rm \
-    --name swarmui \
-    --mount source=swarmdata,target=/Data \
-    --mount source=swarmbackend,target=/dlbackend \
-    -v ./Models:/Models \
-    -v ./Output:/Output \
-    -v ./src/BuiltinExtensions/ComfyUIBackend/CustomWorkflows:/src/BuiltinExtensions/ComfyUIBackend/CustomWorkflows \
-    --gpus=all -p 7801:7801 swarmui
+./launchtools/launch-docker.sh
