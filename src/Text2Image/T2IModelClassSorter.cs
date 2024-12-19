4e10c53d7bb44958f711f496cc4dc8e6a5d2302d
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-12-19 08:02:39 -0800

    autodetect hunyuan video vae

--- src/Text2Image/T2IModelClassSorter.cs
+++ src/Text2Image/T2IModelClassSorter.cs
@@ -75,6 +75,7 @@ public class T2IModelClassSorter
         bool isLtxv(JObject h) => h.ContainsKey("model.diffusion_model.adaln_single.emb.timestep_embedder.linear_1.bias");
         bool isSana(JObject h) => h.ContainsKey("attention_y_norm.weight") && h.ContainsKey("blocks.0.attn.proj.weight");
         bool isHunyuanVideo(JObject h) => h.ContainsKey("model.model.txt_in.individual_token_refiner.blocks.1.self_attn.qkv.weight");
+        bool isHunyuanVideoVae(JObject h) => h.ContainsKey("decoder.conv_in.conv.bias");
         // ====================== Stable Diffusion v1 ======================
         Register(new() { ID = "stable-diffusion-v1", CompatClass = "stable-diffusion-v1", Name = "Stable Diffusion v1", StandardWidth = 512, StandardHeight = 512, IsThisModelOfClass = (m, h) =>
         {
@@ -319,11 +320,11 @@ public class T2IModelClassSorter
         }});
         Register(new() { ID = "hunyuan-video", CompatClass = "hunyuan-video", Name = "Hunyuan Video", StandardWidth = 848, StandardHeight = 480, IsThisModelOfClass = (m, h) =>
         {
-            return isHunyuanVideo(h); // TODO
+            return isHunyuanVideo(h);
         }});
         Register(new() { ID = "hunyuan-video/vae", CompatClass = "hunyuan-video", Name = "Hunyuan Video VAE", StandardWidth = 848, StandardHeight = 480, IsThisModelOfClass = (m, h) =>
         {
-            return false; // TODO
+            return isHunyuanVideoVae(h);
         }});
         Register(new() { ID = "nvidia-sana-1600", CompatClass = "nvidia-sana-1600", Name = "NVIDIA Sana 1600M", StandardWidth = 1024, StandardHeight = 1024, IsThisModelOfClass = (m, h) =>
         {
