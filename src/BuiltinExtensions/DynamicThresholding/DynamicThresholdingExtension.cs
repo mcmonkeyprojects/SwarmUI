09d4f5eecf5c7dd69792be2ebffcb17409c49547
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-12-23 05:16:53 -0800

    minor improvements to dynthresh code

--- src/BuiltinExtensions/DynamicThresholding/DynamicThresholdingExtension.cs
+++ src/BuiltinExtensions/DynamicThresholding/DynamicThresholdingExtension.cs
@@ -15,6 +15,8 @@ public class DynamicThresholdingExtension : Extension
 
     public static T2IRegisteredParam<bool> SeparateFeatureChannels;
 
+    public static T2IParamGroup DynThreshGroup;
+
     public override void OnInit()
     {
         // Add the installable info for the comfy node backend
@@ -24,46 +26,46 @@ public class DynamicThresholdingExtension : Extension
         // Track a feature ID that will automatically be enabled for a backend if the comfy node is installed
         ComfyUIBackendExtension.NodeToFeatureMap["DynamicThresholdingFull"] = "dynamic_thresholding";
         // Build the actual parameters list for inside SwarmUI, note the usage of the feature flag defined above
-        T2IParamGroup dynThreshGroup = new("Dynamic Thresholding", Toggles: true, Open: false, IsAdvanced: true);
+        DynThreshGroup = new("Dynamic Thresholding", Toggles: true, Open: false, IsAdvanced: true);
         MimicScale = T2IParamTypes.Register<double>(new("[DT] Mimic Scale", "[Dynamic Thresholding]\nMimic Scale value (target for the CFG Scale recentering).",
-            "7", Min: 0, Max: 100, Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 1,
+            "7", Min: 0, Max: 100, Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 1,
             Examples: ["5", "7", "9"]
             ));
         ThresholdPercentile = T2IParamTypes.Register<double>(new("[DT] Threshold Percentile", "[Dynamic Thresholding]\nthresholding percentile. '1' disables, '0.95' is decent value for enabled.",
-            "1", Min: 0, Max: 1, Step: 0.05, Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 2,
+            "1", Min: 0, Max: 1, Step: 0.05, Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 2,
             Examples: ["1", "0.99", "0.95", "0.9"]
             ));
         CFGScaleMode = T2IParamTypes.Register<string>(new("[DT] CFG Scale Mode", "[Dynamic Thresholding]\nMode for the CFG Scale scheduler.",
-            "Constant", Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 3,
+            "Constant", Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 3,
             GetValues: (_) => ["Constant", "Linear Down", "Half Cosine Down", "Cosine Down", "Linear Up", "Half Cosine Up", "Cosine Up", "Power Up", "Power Down", "Linear Repeating", "Cosine Repeating"]
             ));
         CFGScaleMin = T2IParamTypes.Register<double>(new("[DT] CFG Scale Minimum", "[Dynamic Thresholding]\nCFG Scale minimum value (for non-constant CFG mode).",
-            "0", Min: 0, Max: 100, Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 4,
+            "0", Min: 0, Max: 100, Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 4,
             Examples: ["0", "1", "2", "5"]
             ));
         MimicScaleMode = T2IParamTypes.Register<string>(new("[DT] Mimic Scale Mode", "[Dynamic Thresholding]\nMode for the Mimic Scale scheduler.",
-            "Constant", Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 5,
+            "Constant", Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 5,
             GetValues: (_) => ["Constant", "Linear Down", "Half Cosine Down", "Cosine Down", "Linear Up", "Half Cosine Up", "Cosine Up", "Power Up", "Power Down", "Linear Repeating", "Cosine Repeating"]
             ));
         MimicScaleMin = T2IParamTypes.Register<double>(new("[DT] Mimic Scale Minimum", "[Dynamic Thresholding]\nMimic Scale minimum value (for non-constant mimic mode).",
-            "0", Min: 0, Max: 100, Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 6,
+            "0", Min: 0, Max: 100, Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 6,
             Examples: ["0", "1", "2", "5"]
             ));
         SchedulerValue = T2IParamTypes.Register<double>(new("[DT] Scheduler Value", "[Dynamic Thresholding]\nIf either scale scheduler is 'Power', this is the power factor.\nIf using 'repeating', this is the number of repeats per image. Otherwise, it does nothing.",
-            "4", Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 7,
+            "4", Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 7,
             Examples: ["2", "4", "8"]
             ));
         SeparateFeatureChannels = T2IParamTypes.Register<bool>(new("[DT] Separate Feature Channels", "[Dynamic Thresholding]\nWhether to separate the feature channels.\nNormally leave this on. I think it should be off for RCFG?",
-            "true", Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 8
+            "true", Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 8
             ));
         ScalingStartpoint = T2IParamTypes.Register<string>(new("[DT] Scaling Startpoint", "[Dynamic Thresholding]\nWhether to scale relative to the mean value or to zero.\nUse 'MEAN' normally. If you want RCFG logic, use 'ZERO'.",
-            "MEAN", Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 9, GetValues: (_) => ["MEAN", "ZERO"]
+            "MEAN", Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 9, GetValues: (_) => ["MEAN", "ZERO"]
             ));
         VariabilityMeasure = T2IParamTypes.Register<string>(new("[DT] Variability Measure", "[Dynamic Thresholding]\nWhether to use standard deviation ('STD') or thresholded absolute values ('AD').\nNormally use 'AD'. Use 'STD' if wanting RCFG logic.",
-            "AD", Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 10, GetValues: (_) => ["AD", "STD"]
+            "AD", Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 10, GetValues: (_) => ["AD", "STD"]
             ));
         InterpolatePhi = T2IParamTypes.Register<double>(new("[DT] Interpolate Phi", "[Dynamic Thresholding]\n'phi' interpolation factor.\nInterpolates between original value and DT value, such that 0.0 = use original, and 1.0 = use DT.\n(This exists because RCFG is bad and so half-removing it un-breaks it - better to just not do RCFG).",
-            "1", Min: 0, Max: 1, Step: 0.05, Group: dynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 11,
+            "1", Min: 0, Max: 1, Step: 0.05, Group: DynThreshGroup, FeatureFlag: "dynamic_thresholding", OrderPriority: 11,
             Examples: ["0", "0.25", "0.5", "0.75", "1"]
             ));
         // The actual implementation: add the node as a step in the workflow generator.
@@ -94,7 +96,7 @@ public class DynamicThresholdingExtension : Extension
                 });
                 // Workflow additions generally only do anything if a key passthrough field is updated.
                 // In our case, we're replacing the Model node, so update FinalModel to point at our node's output.
-                g.FinalModel = [$"{newNode}", 0];
+                g.FinalModel = [newNode, 0];
             }
             // See WorkflowGeneratorSteps for definition of the priority values.
             // -5.5 puts this before the main Sampler, but after most other model changes (eg controlnet)
