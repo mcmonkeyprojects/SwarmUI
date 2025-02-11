e4b54b6f2486007913e965135007e735a18ebb80
yoinked <pepe.dannyboy@gmail.com>
2025-02-10 17:34:17 -0800

    okay maybe (sorry for fucking up the git thing

--- src/WebAPI/API.cs
+++ src/WebAPI/API.cs
@@ -8,8 +8,6 @@ using SwarmUI.Utils;
 using System.IO;
 using System.Net.WebSockets;
 using System.Reflection;
-using System.Text;
-using System.Collections.Concurrent;
 
 namespace SwarmUI.WebAPI;
 
@@ -46,7 +44,7 @@ public class API
             if (context.Request.Method == "OPTIONS")
             {
                 context.Response.Headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS";
-                context.Response.Headers["Access-Control-Allow-Headers"] = "Content-Type, *"; // probably needs further refining, oh well
+                context.Response.Headers["Access-Control-Allow-Headers"] = "Content-Type"; 
                 context.Response.Headers["Access-Control-Max-Age"] = "3600";
                 context.Response.StatusCode = 204; 
                 return; 
