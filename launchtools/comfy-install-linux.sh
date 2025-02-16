62443d2ae2ca2906cb105dd4c7e75b0dba7693cf
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-02-16 13:27:45 -0800

    cleanup linux install echos

--- launchtools/comfy-install-linux.sh
+++ launchtools/comfy-install-linux.sh
@@ -2,7 +2,7 @@
 
 # Check if GPU type is provided
 if [ $# -eq 0 ]; then
-    echo "Error: GPU type not specified. Please use 'amd' or 'nv' as an argument."
+    >&2 echo "Error: GPU type not specified. Please use 'amd' or 'nv' as an argument."
     exit 1
 fi
 
@@ -10,7 +10,7 @@ GPU_TYPE=$1
 
 # Validate GPU type
 if [ "$GPU_TYPE" != "amd" ] && [ "$GPU_TYPE" != "nv" ]; then
-    echo "Error: Invalid GPU type. Please use 'amd' or 'nv'."
+    >&2 echo "Error: Invalid GPU type. Please use 'amd' or 'nv'."
     exit 1
 fi
 
@@ -31,8 +31,8 @@ do
     fi
 done
 if [ "$python" == "" ]; then
-    >&2 echo ERROR: cannot find python3
-    >&2 echo Please follow the install instructions in the readme!
+    >&2 echo "ERROR: cannot find python3"
+    >&2 echo "Please follow the install instructions in the readme!"
     exit 1
 fi
 
@@ -43,27 +43,33 @@ case $venv in
         :
     ;;
     *)
-        >&2 echo ERROR: python venv is not installed
-        >&2 echo Please follow the install instructions in the readme!
-        >&2 echo If on Ubuntu/Debian, you may need: sudo apt install python3-venv
+        >&2 echo "ERROR: python venv is not installed"
+        >&2 echo "Please follow the install instructions in the readme!"
+        >&2 echo "If on Ubuntu/Debian, you may need: sudo apt install python3-venv"
         exit 1
     ;;
 esac
 
 # Make and activate the venv. "python3" in the venv is now the python executable.
 if [ -z "${SWARM_NO_VENV}" ]; then
+    echo "Making venv..."
     $python -s -m venv venv
     source venv/bin/activate
     python=python3
+else
+    echo "swarm_no_venv set, will not create venv"
 fi
 
 # Install PyTorch based on GPU type
 if [ "$GPU_TYPE" == "nv" ]; then
+    echo "install nvidia torch..."
     $python -s -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu124
 elif [ "$GPU_TYPE" == "amd" ]; then
+    echo "install amd torch..."
     $python -s -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.1
 fi
 
+echo "install general requirements..."
 $python -s -m pip install -r requirements.txt
 
 echo "Installation completed for $GPU_TYPE GPU."
