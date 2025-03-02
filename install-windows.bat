88276a03df8f9f5a2d713507c734bc86a98afb3a
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-03-02 07:24:11 -0800

    install-windows: dotnet sdk check too

--- install-windows.bat
+++ install-windows.bat
@@ -14,10 +14,19 @@ if exist SwarmUI.sln (
     exit /b
 )
 
-winget install Microsoft.DotNet.SDK.8 --accept-source-agreements --accept-package-agreements
+set "tempfile=%TEMP%\swarm_dotnet_sdklist.tmp"
+dotnet --list-sdks > "%tempfile%"
+findstr "8.0." "%tempfile%" > nul
+if %ERRORLEVEL% neq 0 (
+    echo DotNet SDK 8 is not installed, will install from WinGet...
+    winget install Microsoft.DotNet.SDK.8 --accept-source-agreements --accept-package-agreements
+)
+del "%tempfile%"
 
 WHERE git
-IF %ERRORLEVEL% NEQ 0 winget install --id Git.Git -e --source winget --accept-source-agreements --accept-package-agreements
+IF %ERRORLEVEL% NEQ 0 (
+    winget install --id Git.Git -e --source winget --accept-source-agreements --accept-package-agreements
+)
 
 git clone https://github.com/mcmonkeyprojects/SwarmUI
 cd SwarmUI
