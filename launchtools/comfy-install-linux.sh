2a3b12f0d6e0e03c0b06566171f7271d61e6304f
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-03-11 01:07:56 -0700

    Linux: default install rocm 6.2.4

--- launchtools/comfy-install-linux.sh
+++ launchtools/comfy-install-linux.sh
@@ -66,7 +66,7 @@ if [ "$GPU_TYPE" == "nv" ]; then
     $python -s -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu124
 elif [ "$GPU_TYPE" == "amd" ]; then
     echo "install amd torch..."
-    $python -s -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.1
+    $python -s -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2.4
 fi
 
 echo "install general requirements..."
