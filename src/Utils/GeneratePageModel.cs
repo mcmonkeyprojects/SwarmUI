21e002e8d9982e3eee28d49c28d8f5da4afbeb7a
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-03-29 18:33:19 -0700

    split up the cshtml

---
+++ src/Utils/GeneratePageModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using SwarmUI.Accounts;
+using SwarmUI.Core;
+
+namespace SwarmUI.Utils;
+
+public class GeneratePageModel
+{
+    public HttpContext HttpContext;
+
+    public string AlwaysRefreshOnLoad => Program.ServerSettings.Backends.AlwaysRefreshOnLoad ? "true" : "false";
+
+    public string ExperimentalHide => Program.ServerSettings.ShowExperimentalFeatures ? "" : "secretexperimental";
+
+    public string InstanceTitle => Program.ServerSettings.UserAuthorization.InstanceTitle;
+
+    public bool IsLoginEnabled => Program.ServerSettings.UserAuthorization.AuthorizationRequired;
+
+    public User User;
+    
+    public GeneratePageModel(HttpContext context)
+    {
+        HttpContext = context;
+        User = IsLoginEnabled ? WebUtil.GetValidLogin(HttpContext) : null;
+    }
+}
