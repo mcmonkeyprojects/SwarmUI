efeb9f5626028877b6e22bb036e70eb29c07bad6
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-03-31 07:40:43 -0700

    drop legacy docker script

--- launch-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo WARNING: Deprecated legacy script. Will be removed soon. Use "./launchtools/launch-standard-docker.sh" instead.
-
-sleep 15
-
-./launchtools/launch-standard-docker.sh
