5ff59bb25803443713a8f97a9ef7aeebaa022c77
Odls <alexeatapple@yahoo.com.tw>
2025-07-29 17:04:36 +0800

    Use LoadImage().load_image to replaced the redundant function copy.

--- src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyCommon/SwarmInputNodes.py
+++ src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyCommon/SwarmInputNodes.py
@@ -1,12 +1,7 @@
 from . import SwarmLoadImageB64
 import folder_paths
-from nodes import CheckpointLoaderSimple
+from nodes import CheckpointLoaderSimple, LoadImage
 import os
-
-import node_helpers
-from PIL import Image, ImageOps, ImageSequence
-import numpy as np
-import torch
 
 INT_MAX = 0xffffffffffffffff
 INT_MIN = -INT_MAX
@@ -192,53 +187,6 @@ class SwarmInputBoolean:
     def do_input(self, value, **kwargs):
         return (value, )
 
-def load_image(image):
-    image_path = folder_paths.get_annotated_filepath(image)
-
-    img = node_helpers.pillow(Image.open, image_path)
-
-    output_images = []
-    output_masks = []
-    w, h = None, None
-
-    excluded_formats = ['MPO']
-
-    for i in ImageSequence.Iterator(img):
-        i = node_helpers.pillow(ImageOps.exif_transpose, i)
-
-        if i.mode == 'I':
-            i = i.point(lambda i: i * (1 / 255))
-        image = i.convert("RGB")
-
-        if len(output_images) == 0:
-            w = image.size[0]
-            h = image.size[1]
-
-        if image.size[0] != w or image.size[1] != h:
-            continue
-
-        image = np.array(image).astype(np.float32) / 255.0
-        image = torch.from_numpy(image)[None,]
-        if 'A' in i.getbands():
-            mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
-            mask = 1. - torch.from_numpy(mask)
-        elif i.mode == 'P' and 'transparency' in i.info:
-            mask = np.array(i.convert('RGBA').getchannel('A')).astype(np.float32) / 255.0
-            mask = 1. - torch.from_numpy(mask)
-        else:
-            mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-        output_images.append(image)
-        output_masks.append(mask.unsqueeze(0))
-
-    if len(output_images) > 1 and img.format not in excluded_formats:
-        output_image = torch.cat(output_images, dim=0)
-        output_mask = torch.cat(output_masks, dim=0)
-    else:
-        output_image = output_images[0]
-        output_mask = output_masks[0]
-
-    return (output_image, output_mask)
-
 class SwarmInputImage:
     DEFAULT_VALUE_TEXT = "(Do Not Set Me)"
     @classmethod
@@ -265,7 +213,7 @@ class SwarmInputImage:
 
     def do_input(self, value=None, image=None, **kwargs):
         if not value or value==self.DEFAULT_VALUE_TEXT:
-            return load_image(image)
+            return LoadImage().load_image(image)
         else:
             return SwarmLoadImageB64.b64_to_img_and_mask(value)
 
