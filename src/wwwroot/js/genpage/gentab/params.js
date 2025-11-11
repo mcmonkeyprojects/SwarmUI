c3e33487cfd840d9717836850d49d3b97d39e129
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-11-10 21:56:14 -0800

    Reset Params button: clear prompt image

--- src/wwwroot/js/genpage/gentab/params.js
+++ src/wwwroot/js/genpage/gentab/params.js
@@ -1139,6 +1139,7 @@ function resetParamsToDefault(exclude = [], doDefaultPreset = true) {
     if (aspect) { // Fix resolution trick incase the reset broke it
         triggerChangeFor(aspect);
     }
+    clearPromptImages();
     currentModelChanged();
     clearPresets();
     let defaultPreset = getPresetByTitle('default');
