3ecf4a701392dad31fd1b3578fffb9560dcf8763
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-11-26 08:11:11 -0800

    explicit require higher transformers min vers

--- src/BuiltinExtensions/ComfyUIBackend/ComfyUISelfStartBackend.cs
+++ src/BuiltinExtensions/ComfyUIBackend/ComfyUISelfStartBackend.cs
@@ -677,7 +677,7 @@ public class ComfyUISelfStartBackend : ComfyUIAPIAbstractBackend
         ("yarl", "yarl", ">=", "1.18.0"),
         ("av", "av", ">=", "14.2.0"),
         ("spandrel", "spandrel", ">=", "0.4.1"),
-        ("transformers", "transformers", ">=", "4.37.2"),
+        ("transformers", "transformers", ">=", "4.50.3"),
         ("ultralytics", "ultralytics", "==", "8.3.197"), // This is hard-pinned due to the malicious 8.3.41 incident, only manual updates when needed until security practices are improved.
         ("pip", "pip", ">=", "25.0") // Don't need latest, just can't be too old, this is mostly just here for a sanity check.
     ];
