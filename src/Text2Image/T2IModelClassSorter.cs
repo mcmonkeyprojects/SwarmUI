9c2bb0f00564f84843fb1ee3ae52455cadbb3d24
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-11-28 12:05:38 -0800

    detect zimage loras

--- src/Text2Image/T2IModelClassSorter.cs
+++ src/Text2Image/T2IModelClassSorter.cs
@@ -143,6 +143,7 @@ public class T2IModelClassSorter
         bool isCosmosPredict2_14B(JObject h) => h.ContainsKey("net.blocks.0.adaln_modulation_cross_attn.1.weight");
         bool isLumina2(JObject h) => h.ContainsKey("model.diffusion_model.cap_embedder.0.weight") || h.ContainsKey("cap_embedder.0.weight");
         bool isZImage(JObject h) => h.ContainsKey("model.diffusion_model.context_refiner.0.attention.k_norm.weight") || h.ContainsKey("context_refiner.0.attention.k_norm.weight");
+        bool isZImageLora(JObject h) => h.ContainsKey("diffusion_model.layers.0.adaLN_modulation.0.lora_A.weight") && h.ContainsKey("diffusion_model.layers.9.feed_forward.w3.lora_B.weight");
         bool tryGetWanTok(JObject h, out JToken tok) => h.TryGetValue("model.diffusion_model.blocks.0.cross_attn.k.bias", out tok) || h.TryGetValue("blocks.0.cross_attn.k.bias", out tok) || h.TryGetValue("lora_unet_blocks_0_cross_attn_k.lora_down.weight", out tok);
         bool tryGetPatchEmbedTok(JObject h, out JToken tok) => h.TryGetValue("patch_embedding.weight", out tok) || h.TryGetValue("model.diffusion_model.patch_embedding.weight", out tok);
         bool isWan21_1_3b(JObject h) => tryGetWanTok(h, out JToken tok) && tok["shape"].ToArray()[0].Value<long>() == 1536;
@@ -517,7 +518,7 @@ public class T2IModelClassSorter
         }});
         Register(new() { ID = "z-image/lora", CompatClass = CompatZImage, Name = "Z-Image LoRA", StandardWidth = 1024, StandardHeight = 1024, IsThisModelOfClass = (m, h) =>
         {
-            return false;
+            return isZImageLora(h);
         }});
         // ====================== Random Other Models ======================
         Register(new() { ID = "chroma", CompatClass = CompatChroma, Name = "Chroma", StandardWidth = 1024, StandardHeight = 1024, IsThisModelOfClass = (m, h) =>
