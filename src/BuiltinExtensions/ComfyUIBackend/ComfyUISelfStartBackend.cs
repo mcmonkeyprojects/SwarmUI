9ec5fb0aaa0212b91f0d78f9e2d73d9c9f17a41c
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-12-28 11:54:37 -0800

    bump transformers depend version

--- src/BuiltinExtensions/ComfyUIBackend/ComfyUISelfStartBackend.cs
+++ src/BuiltinExtensions/ComfyUIBackend/ComfyUISelfStartBackend.cs
@@ -115,7 +115,7 @@ public class ComfyUISelfStartBackend : ComfyUIAPIAbstractBackend
         ("yarl", "yarl", ">=", "1.18.0"),
         ("av", "av", ">=", "14.2.0"),
         ("spandrel", "spandrel", ">=", "0.4.1"),
-        ("transformers", "transformers", ">=", "4.50.3"),
+        ("transformers", "transformers", ">=", "4.57.3"),
         ("ultralytics", "ultralytics", "==", "8.3.197"), // This is hard-pinned due to the malicious 8.3.41 incident, only manual updates when needed until security practices are improved.
         ("pip", "pip", ">=", "25.0") // Don't need latest, just can't be too old, this is mostly just here for a sanity check.
     ];
