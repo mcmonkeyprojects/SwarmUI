479c75cff8b10c1f6183ad4057312c10408ef759
bigcletus <vaughncaleb207@yahoo.com>
2026-04-18 20:38:05 -0400

    Update launch-standard-docker.sh
    Added options to change between AMD and NVidia. Defaults to NVidia

--- launchtools/launch-standard-docker.sh
+++ launchtools/launch-standard-docker.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
-GPU=="nvidia"
+
 # Note: This is an example file, do not edit `launch-standard-docker.sh`. Instead, duplicate the file and edit your duplicate.
 # `custom-launch-docker.sh` is reserved in gitignore for if you want to use that.
 
 # Run script automatically in Swarm's dir regardless of how it was triggered
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/.."
-if [[ "$1" == "amd" ]]
+
+# Set default GPU
+GPU="nvidia"
+
+if [[ $1 == "amd" ]]
 then
-    GPU=="amd"
-    docker build --build-arg UID=$UID -f launchtools/AMDGPU-StandardDockerfile.docker -t swarmui .
+        GPU="rocm"
+        docker build --build-arg UID=$UID -f launchtools/AMDGPU-StandardDockerfile.docker -t swarmui-rocm .
+else
+        docker build --build-arg UID=$UID -f launchtools/StandardDockerfile.docker -t swarmui-nvidia .
 fi
 
-if [[ "$1" == "nvidia" ]]
+if [[ $1 != "fixch" ]]
 then
-    docker build --build-arg UID=$UID -f launchtools/StandardDockerfile.docker -t swarmui .
+        shift # Remove the GPU ID arg to prevent bugs
 fi
 # Run this script with 'fixch' to run as root in the container and chown to the correct user
 SETUSER="--user $UID:$(id -g) --cap-drop=ALL"
-# TODO: Validate arg spacing handling
-POSTARG="--forward_restart $@"
+POSTARG=("--forward_restart" "$@")
 if [[ "$1" == "fixch" ]]
 then
     SETUSER=""
-    POSTARG="fixch $UID"
+    POSTARG=("fixch" "$UID")
 fi
 # add "--network=host" if you want to access other services on the host network (eg a separated comfy instance)
 docker run -it \
@@ -37,7 +42,7 @@ docker run -it \
     -v "$PWD/Models:/SwarmUI/Models" \
     -v "$PWD/Output:/SwarmUI/Output" \
     -v "$PWD/src/BuiltinExtensions/ComfyUIBackend/CustomWorkflows:/SwarmUI/src/BuiltinExtensions/ComfyUIBackend/CustomWorkflows" \
-    --gpus=all -p 7801:7801 swarmui "$POSTARG"
+    --gpus=all -p 7801:7801 swarmui-"$GPU" "${POSTARG[@]}"
 
 if [ $? == 42 ]; then
     exec "$SCRIPT_DIR/launch-standard-docker.sh" "$@"
