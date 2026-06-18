3bf264fdb4a8cbf021aceb6f9215e93a9ece8992
Juan Treminio <jtreminio@gmail.com>
2026-06-18 07:22:49 -0600

    Allow user to override sigma shift

--- src/BuiltinExtensions/ComfyUIBackend/WorkflowGeneratorModelSupport.cs
+++ src/BuiltinExtensions/ComfyUIBackend/WorkflowGeneratorModelSupport.cs
@@ -1133,6 +1133,16 @@ public partial class WorkflowGenerator
         {
             helpers.LoadClip("boogu", helpers.GetQwen3vl_8bModel());
             helpers.DoVaeLoader(UserInput.SourceSession?.User?.Settings?.VAEs?.DefaultFluxVAE, "flux-1", "flux-ae");
+            double shift = UserInput.Get(T2IParamTypes.SigmaShift, 3.16, sectionId: sectionId);
+            if (shift > 0)
+            {
+                string samplingNode = CreateNode("ModelSamplingAuraFlow", new JObject()
+                {
+                    ["model"] = LoadingModel,
+                    ["shift"] = shift
+                });
+                LoadingModel = [samplingNode, 0];
+            }
         }
         else if (IsFlux() && (LoadingClip is null || LoadingVAE is null || UserInput.Get(T2IParamTypes.T5XXLModel) is not null || UserInput.Get(T2IParamTypes.ClipLModel) is not null))
         {
