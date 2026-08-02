01aafa6c704dbb2efe81a34dda3be073a4899ac9
Juan Treminio <jtreminio@gmail.com>
2026-08-02 15:15:32 -0500

    Fix upstream break; unwrap video stream

--- src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyCommon/SwarmKSampler.py
+++ src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyCommon/SwarmKSampler.py
@@ -141,6 +141,8 @@ def make_swarm_sampler_callback(steps, device, model, previews):
     def callback(step, x0, x, total_steps):
         pbar.update_absolute(step + 1, total_steps, None)
         if previewer:
+            if getattr(x0, "is_nested", False) and hasattr(x0, "tensors"):
+                x0 = x0.tensors[0]
             if x0.ndim == 5:
                 # video shape is [batch, channels, backwards time, width, height], for previews needs to be swapped to [forwards time, channels, width, height]
                 x0 = x0[0].permute(1, 0, 2, 3)
